2f2a058ed3bc1be634e2ff22bc50360260affaa6
安思琪 <ansiqi@innotechx.com>
2020-10-15 23:41:35 +0800

    update 10/16 Java基础学习进度

--- LXFBook/src/liaoxuefeng/com/Main.java
+++ LXFBook/src/liaoxuefeng/com/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Person ming = new Person();
-        ming.setBirth(2008);
 
-        System.out.println(ming.getAge());
+        System.out.println(ming.getName());
     }
 }
